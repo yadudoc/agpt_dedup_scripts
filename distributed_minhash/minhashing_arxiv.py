9403be473b727570b0d69fbde25387c8869fcf0e
Yadu Nand B <yadudoc1729@gmail.com>
2025-09-03 19:50:48 +0000

    Adding distributed minhashing scripts

---
+++ distributed_minhash/minhashing_arxiv.py
@@ -0,0 +1,50 @@
+import parsl
+import argparse
+import os
+import glob
+from parsl import python_app
+import pathlib
+from polaris_multinode import config
+
+
+@python_app(cache=True)
+def minhash_file(infile: str, output_dir: str, num_perm: int) -> float:
+    from deduplication.minhash import compute_minhash_for_file
+    import time
+    start = time.perf_counter()
+    compute_minhash_for_file(infile, output_dir, num_perm)
+    done = time.perf_counter()
+    return infile, done-start
+
+
+def minhash_dir(input_dir: str, output_dir: str, num_perm: int = 128):
+
+    pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
+    futures = []
+    for jsonl_file in glob.glob(input_dir + "/*json*"):
+        print(f"Processing: {jsonl_file}")
+        out_dir = f"{output_dir}/{jsonl_file.split('/')[-1]}"
+        os.makedirs(out_dir, exist_ok=True)
+        print(f"Writing output to {out_dir}")
+        future = minhash_file(jsonl_file, out_dir, num_perm=128)
+        futures.append(future)
+
+    for fu in futures:
+        infile, time = fu.result()
+        print(f"Processed {infile} in {time:.2f}s")
+    print("Done processing all files")
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_dir", required=True,
+                        help="Input directory with jsonl files")
+    parser.add_argument("--output_dir", required=True,
+                        help="Output directory to write pickle output files")
+    parser.add_argument("--num_perm", required=True,
+                        help="Number of permutations in the minhash calculation")
+    args = parser.parse_args()
+
+    parsl.load(config)
+    minhash_dir(args.input_dir, args.output_dir, args.num_perm)
+    
---
+++ distributed_minhash/polaris_multinode_large.py
@@ -0,0 +1,57 @@
+from parsl.config import Config
+
+# PBSPro is the right provider for Polaris:
+from parsl.providers import PBSProProvider
+# The high throughput executor is for scaling to HPC systems:
+from parsl.executors import HighThroughputExecutor
+# Use the MPI launcher to create one worker per GPU
+from parsl.launchers import MpiExecLauncher
+# For checkpointing:
+from parsl.utils import get_all_checkpoints
+
+# Adjust your user-specific options here:
+from parsl.utils import get_all_checkpoints
+
+
+
+user_opts = {
+    "worker_init":      f"source /home/yadunand/setup_agpt_env_3.sh", # load the environment where parsl is installed
+    "scheduler_options":"#PBS -l filesystems=home:eagle" , # specify any PBS options here, like filesystems
+    "account":          "argonne_tpc",
+    "queue":            "preemptable",
+    "walltime":         "1:00:00",
+}
+
+
+config = Config(
+        executors=[
+            HighThroughputExecutor(
+                label="htex",
+                heartbeat_period=15,
+                heartbeat_threshold=120,
+                worker_debug=True,
+                max_workers_per_node=4,
+                # This give optimal binding of threads to GPUs on a Polaris node
+                provider=PBSProProvider(
+                    launcher=MpiExecLauncher(bind_cmd="--cpu-bind", overrides="--depth=64 --ppn 1"),
+                    account=user_opts["account"],
+                    queue=user_opts["queue"],
+                    select_options="ngpus=4",
+                    # PBS directives (header lines)
+                    scheduler_options=user_opts["scheduler_options"],
+                    # Command to be run before starting a worker, such as:
+                    worker_init=user_opts["worker_init"],
+                    # number of compute nodes allocated for each block
+                    nodes_per_block=10,
+                    init_blocks=1,
+                    min_blocks=0,
+                    max_blocks=1, # Can increase more to have more parallel jobs
+                    walltime=user_opts["walltime"]
+                ),
+            ),
+        ],
+        checkpoint_mode='task_exit',
+        checkpoint_files=get_all_checkpoints(),
+        retries=2,
+        app_cache=True,
+)
